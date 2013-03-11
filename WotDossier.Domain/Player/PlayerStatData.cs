bad501d61e37cf9746b541a7c221f70a75a370af
Rembel@gmail.com <Rembel@users.noreply.github.com>
2013-03-11 22:10:44 +0000

    Common tab update

--- WotDossier.Domain/Player/PlayerStatData.cs
+++ WotDossier.Domain/Player/PlayerStatData.cs
@@ -4,14 +4,14 @@ namespace WotDossier.Domain.Player
 {
     public class PlayerStatData
     {
-        public string name;
-        public double created_at;
-        public double updated_at;
+        public string name { get; set; }
+        public double created_at { get; set; }
+        public double updated_at { get; set; }
         public Achievements achievements { get; set; }
         public Battles battles { get; set; }
         public Summary summary { get; set; }
         public Experience experience { get; set; }
         public Ratings ratings { get; set; }
-        public IList<Vehicle> vehicles;
+        public IList<Vehicle> vehicles { get; set; }
     }
 }
---
+++ WotDossier/Converters/DoubleToDateConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace WotDossier.Converters
+{
+    public class DoubleToDateConverter : IValueConverter
+    {
+        private static readonly DoubleToDateConverter _defaultInstance = new DoubleToDateConverter();
+
+        public static DoubleToDateConverter Default { get { return _defaultInstance; } }
+
+        /// <summary>
+        /// Converts a value. 
+        /// </summary>
+        /// <returns>
+        /// A converted value. If the method returns null, the valid null value is used.
+        /// </returns>
+        /// <param name="value">The value produced by the binding source.</param><param name="targetType">The type of the binding target property.</param><param name="parameter">The converter parameter to use.</param><param name="culture">The culture to use in the converter.</param>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            DateTime date = new DateTime(1970, 1, 1).AddSeconds((long)(double)value);
+            return date.ToString("dd.MM.yyyy");
+        }
+
+        /// <summary>
+        /// Converts a value. 
+        /// </summary>
+        /// <returns>
+        /// A converted value. If the method returns null, the valid null value is used.
+        /// </returns>
+        /// <param name="value">The value that is produced by the binding target.</param><param name="targetType">The type to convert to.</param><param name="parameter">The converter parameter to use.</param><param name="culture">The culture to use in the converter.</param>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

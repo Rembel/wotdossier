665aa20034204f1496794c96e65abc8daedd2029
Rembel@gmail.com <Rembel@users.noreply.github.com>
2013-03-13 15:47:33 +0000

    Common tab update

---
+++ WotDossier.Common/TypeHelper.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace WotDossier.Common
+{
+    public class TypeHelper
+    {
+        private static PropertyInfo GetPropertyInternal(LambdaExpression p)
+        {
+            MemberExpression memberExpression;
+
+            if (p.Body is UnaryExpression)
+            {
+                UnaryExpression ue = (UnaryExpression)p.Body;
+                memberExpression = (MemberExpression)ue.Operand;
+            }
+            else
+            {
+                memberExpression = (MemberExpression)p.Body;
+            }
+
+            return (PropertyInfo)(memberExpression).Member;
+        }
+
+        public static string GetPropertyName<TObject>(Expression<Func<TObject, object>> p)
+        {
+            return GetPropertyNameInternal(p);
+        }
+
+        public static string GetPropertyName<TObject, T>(Expression<Func<TObject, T>> p)
+        {
+            return GetPropertyNameInternal(p);
+        }
+
+        public static string GetPropertyName<T>(Expression<Func<T>> p)
+        {
+            return GetPropertyNameInternal(p);
+        }
+
+        private static string GetPropertyNameInternal(LambdaExpression p)
+        {
+            return GetPropertyInternal(p).Name;
+        }
+
+        public static PropertyInfo GetProperty<TObject>(Expression<Func<TObject, object>> p)
+        {
+            return GetPropertyInternal(p);
+        }
+
+        public static PropertyInfo GetProperty<TObject, T>(Expression<Func<TObject, T>> p)
+        {
+            return GetPropertyInternal(p);
+        }
+
+        public static PropertyInfo GetProperty<T>(Expression<Func<T>> p)
+        {
+            return GetPropertyInternal(p);
+        }
+    }
+}
---
+++ WotDossier/Converters/DeltaToColorConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace WotDossier.Converters
+{
+    public class DeltaToColorConverter : IValueConverter
+    {
+        private static readonly DeltaToColorConverter defaultInstance = new DeltaToColorConverter();
+
+        public static DeltaToColorConverter Default { get { return defaultInstance; } }
+
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double delta;
+            if(value is int)
+            {
+                delta = (int)value;
+            }
+            else
+            {
+                delta = (double)value;
+            }
+            if (Math.Abs(delta - 0.0) < 0.001)
+                return new SolidColorBrush(Color.FromRgb(186, 191, 186));
+            bool negativBetter = parameter != null ? bool.Parse(parameter.ToString()) : false;
+            if (delta > 0.0 || negativBetter)
+                return Brushes.LimeGreen;
+            return Brushes.Red;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

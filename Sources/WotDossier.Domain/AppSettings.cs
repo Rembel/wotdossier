2355fb16bdceb9086a59d3f2134eb5f85fdb1f95
Rembel@gmail.com <Rembel@users.noreply.github.com>
2013-03-21 15:10:12 +0000

    Russian lang support added

--- Sources/WotDossier.Domain/AppSettings.cs
+++ Sources/WotDossier.Domain/AppSettings.cs
@@ -7,7 +7,14 @@ namespace WotDossier.Domain
 {
     public class AppSettings
     {
+        private string _language = "ru-RU";
         public string PlayerId { get; set; }
         public string Server { get; set; }
+
+        public string Language
+        {
+            get { return _language; }
+            set { _language = value; }
+        }
     }
 }

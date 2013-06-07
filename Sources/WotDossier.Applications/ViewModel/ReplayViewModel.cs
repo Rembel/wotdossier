34bb7f15401c8db0a90b90f0963c191eaafd8b3e
rembel <Rembel@users.noreply.github.com>
2013-06-07 15:25:31 +0000

    Replays manager tab added

---
+++ Sources/WotDossier.Applications/ViewModel/ReplayViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Threading;
+using WotDossier.Applications.View;
+using WotDossier.Dal;
+using WotDossier.Domain.Replay;
+using WotDossier.Domain.Tank;
+using WotDossier.Framework.Applications;
+
+namespace WotDossier.Applications.ViewModel
+{
+    [PartCreationPolicy(CreationPolicy.NonShared)]
+    [Export(typeof(ReplayViewModel))]
+    public class ReplayViewModel : ViewModel<IReplayView>
+    {
+        private ReplayFile _replay;
+
+        public ReplayFile Replay
+        {
+            get { return _replay; }
+            set { _replay = value; }
+        }
+        
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ViewModel&lt;TView&gt;"/> class and
+        /// attaches itself as <c>DataContext</c> to the view.
+        /// </summary>
+        /// <param name="view">The view.</param>
+        [ImportingConstructor]
+        public ReplayViewModel([Import(typeof(IReplayView))]IReplayView view)
+            : base(view)
+        {
+        }
+
+        public void Show()
+        {
+            ViewTyped.Show();
+
+            //convert dossier cache file to json
+            CacheHelper.ReplayToJson(Replay.FileInfo);
+            Thread.Sleep(1000);
+            Replay replay = WotApiClient.Instance.ReadReplay(Replay.FileInfo.FullName.Replace(Replay.FileInfo.Extension, ".json"));
+        }
+    }
+}
--- Sources/WotDossier/Converters/TankClassToImageConverter.cs
+++ Sources/WotDossier/Converters/TankClassToImageConverter.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Globalization;
 using System.Windows.Data;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
 namespace WotDossier.Converters

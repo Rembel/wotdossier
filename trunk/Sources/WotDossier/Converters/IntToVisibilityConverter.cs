f6323e3166cb484aad4078f36b64c8094f11388b
Rembel@gmail.com <Rembel@gmail.com@0b894fdd-9abc-28a4-66d2-b81a71abe1db>
2013-06-16 19:41:51 +0000

    Replays manader update

---
+++ trunk/Sources/WotDossier/Converters/IntToVisibilityConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WotDossier.Converters
+{
+    public class IntToVisibilityConverter : IValueConverter
+    {
+        private static readonly IntToVisibilityConverter defaultInstance = new IntToVisibilityConverter();
+
+        public static IntToVisibilityConverter Default { get { return defaultInstance; } }
+
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (int?)value > 0 ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

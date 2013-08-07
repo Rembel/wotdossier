38323347cbcf0b838ec17aaaf32dd4536fd4ffad
Rembel <Rembel@0b894fdd-9abc-28a4-66d2-b81a71abe1db>
2013-08-07 05:51:12 +0000

    Period Statistic processing updated

---
+++ Sources/WotDossier.Applications/Update/CodeUpdate_2013080602.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlServerCe;
+
+namespace WotDossier.Applications.Update
+{
+    public class CodeUpdate_2013080602 : CodeUpdateBase
+    {
+        private long _version = 2013080602;
+
+        public override long Version
+        {
+            get { return _version; }
+            set { _version = value; }
+        }
+
+        public override void Execute(SqlCeConnection sqlCeConnection, SqlCeTransaction transaction)
+        {
+            TimeSpan utcOffset = TimeZone.CurrentTimeZone.GetUtcOffset(DateTime.Now);
+
+            string commandText = @"Update PlayerStatistic set Updated = DATEADD(hour,@zone,Updated)";
+            SqlCeCommand command = new SqlCeCommand(commandText, sqlCeConnection, transaction);
+
+            command.CommandType = CommandType.Text;
+            command.Parameters.Add("@zone", SqlDbType.Int).Value = utcOffset.Hours;
+
+            command.ExecuteNonQuery();
+        }
+    }
+}

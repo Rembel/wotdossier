393781bee789ecab3a310095efd60ca24b96fab2
rembel <rembel@0b894fdd-9abc-28a4-66d2-b81a71abe1db>
2013-08-27 14:30:24 +0000

    ReplaysManager

---
+++ trunk/Sources/WotDossier.Applications/ViewModel/ReplaysManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.IO;
+using WotDossier.Common;
+using WotDossier.Domain;
+
+namespace WotDossier.Applications.ViewModel
+{
+    [Export]
+    public class ReplaysManager
+    {
+        private const string _dataReplayscatalogXml = @"Data\ReplaysCatalog.xml";
+
+        public void SaveFolder(ReplayFolder replayFolder)
+        {
+            using (StreamWriter writer = File.CreateText(Path.Combine(Environment.CurrentDirectory, _dataReplayscatalogXml)))
+            {
+                writer.WriteLine(XmlSerializer.StoreObjectInXml(replayFolder));
+                writer.Flush();
+            }
+        }
+
+        public List<ReplayFolder> GetFolders()
+        {
+            using (StreamReader streamReader = File.OpenText(Path.Combine(Environment.CurrentDirectory, _dataReplayscatalogXml)))
+            {
+                string tree = streamReader.ReadToEnd();
+                return new List<ReplayFolder> {XmlSerializer.LoadObjectFromXml<ReplayFolder>(tree)};
+            }
+        }
+    }
+}

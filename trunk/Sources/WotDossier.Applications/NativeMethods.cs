9f7603ea96e8d054d28c25102d014d3e590b92a0
rembel <rembel@0b894fdd-9abc-28a4-66d2-b81a71abe1db>
2013-09-11 14:02:28 +0000

    Refactoring

---
+++ trunk/Sources/WotDossier.Applications/NativeMethods.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+
+namespace WotDossier.Applications
+{
+    internal static class NativeMethods
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Globalization", "CA2101:SpecifyMarshalingForPInvokeStringArguments", MessageId = "0"), 
+        System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Globalization", "CA2101:SpecifyMarshalingForPInvokeStringArguments", MessageId = "1"), 
+        System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Globalization", "CA2101:SpecifyMarshalingForPInvokeStringArguments", MessageId = "2"), 
+        DllImport("wininet.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern bool InternetGetCookie(string lpszUrlName, string lpszCookieName, [Out] StringBuilder lpszCookieData, [MarshalAs(UnmanagedType.U4)] out int lpdwSize);
+    }
+}

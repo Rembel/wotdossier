e032ba902a973c216bc067dd5c8674d9b604223e
rembel <Rembel@users.noreply.github.com>
2013-09-25 13:24:50 +0000

    + Performance88 Tab ! Replays files parser replaced

--- Sources/WotDossier.Domain/Replay/Replay.cs
+++ Sources/WotDossier.Domain/Replay/Replay.cs
@@ -1,4 +1,5 @@
 ﻿using System.Runtime.Serialization;
+using WotDossier.Common;
 
 namespace WotDossier.Domain.Replay
 {
@@ -14,5 +15,38 @@ http://wiki.vbaddict.net
         public CommandResult CommandResult { get; set; }
         [DataMember]
         public BattleResult datablock_battle_result { get; set; }
+        [DataMember]
+        public ReplayIdentify identify { get; set; }
+    }
+
+    public class ReplayIdentify
+    {
+        [DataMember]
+        public long accountDBID { get; set; }
+        [DataMember]
+        public long arenaCreateTime { get; set; }
+        [DataMember]
+        public long arenaUniqueID { get; set; }
+        [DataMember]
+        public int countryid { get; set; }
+        [DataMember]
+        public string error { get; set; }
+        [DataMember]
+        public string error_details { get; set; }
+        [DataMember]
+        public long internaluserID { get; set; }
+        [DataMember]
+        public string mapName { get; set; }
+        [DataMember]
+        public int mapid { get; set; }
+        [DataMember]
+        public string playername { get; set; }
+        [DataMember]
+        public int tankid { get; set; }
+
+        public int TankUniqueId()
+        {
+            return Utils.ToUniqueId(countryid, tankid);
+        }
     }
 }

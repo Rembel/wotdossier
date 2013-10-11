217dd8666ba921f1e1e50c9a73440a1e14b2803c
rembel <Rembel@users.noreply.github.com>
2013-10-11 14:11:16 +0000

    redesign

---
+++ Sources/WotDossier.Framework/Controls/AttachedProperties.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace WotDossier.Framework.Controls
+{
+    public class AttachedProperties
+    {
+        public static readonly DependencyProperty UpdateSourceOnKeyProperty =
+    DependencyProperty.RegisterAttached("UpdateSourceOnKey",
+    typeof(Key), typeof(TextBox), new FrameworkPropertyMetadata(Key.None));
+
+        public static void SetUpdateSourceOnKey(UIElement element, Key value)
+        {
+            element.PreviewKeyUp += TextBoxKeyUp;
+            element.SetValue(UpdateSourceOnKeyProperty, value);
+        }
+
+        static void TextBoxKeyUp(object sender, KeyEventArgs e)
+        {
+
+            var textBox = sender as TextBox;
+            if (textBox == null) return;
+
+            var propertyValue = (Key)textBox.GetValue(UpdateSourceOnKeyProperty);
+            if (e.Key != propertyValue) return;
+
+            var bindingExpression = textBox.GetBindingExpression(TextBox.TextProperty);
+            if (bindingExpression != null) bindingExpression.UpdateSource();
+        }
+
+        public static Key GetUpdateSourceOnKey(UIElement element)
+        {
+            return (Key)element.GetValue(UpdateSourceOnKeyProperty);
+        }
+    }
+}

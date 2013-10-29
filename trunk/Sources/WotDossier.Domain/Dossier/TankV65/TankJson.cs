f485cb3e8fb2d0bd4aec5be0e36b76a214d872d8
Rembel <Rembel@0b894fdd-9abc-28a4-66d2-b81a71abe1db>
2013-10-29 19:55:04 +0000

    app crash fix

--- trunk/Sources/WotDossier.Domain/Dossier/TankV65/TankJson.cs
+++ trunk/Sources/WotDossier.Domain/Dossier/TankV65/TankJson.cs
@@ -6,20 +6,92 @@ namespace WotDossier.Domain.Dossier.TankV65
     public class TankJson65
     {
         private IList<IList<string>> _fragsList = new List<IList<string>>();
+        private StatisticJson65 _a15X15 = new StatisticJson65();
+        private StatisticJson65 _clan = new StatisticJson65();
+        private StatisticJson65_2 _clan2 = new StatisticJson65_2();
+        private StatisticJson65 _company = new StatisticJson65();
+        private StatisticJson65_2 _company2 = new StatisticJson65_2();
+        private StatisticJson65 _a7X7 = new StatisticJson65();
+        private AchievementsJson65 _achievements = new AchievementsJson65();
+        private CommonJson65 _common = new CommonJson65();
+        private TotalJson65 _total = new TotalJson65();
+        private MaxJson65 _max15X15 = new MaxJson65();
+        private MaxJson65 _max7X7 = new MaxJson65();
+        private StatisticJson65_2 _a15X152 = new StatisticJson65_2();
 
         /*8.9*/
-        public StatisticJson65 A15x15 { get; set; }
-        public StatisticJson65_2 A15x15_2 { get; set; }
-        public StatisticJson65 Clan { get; set; }
-        public StatisticJson65_2 Clan2 { get; set; }
-        public StatisticJson65 Company { get; set; }
-        public StatisticJson65_2 Company2 { get; set; }
-        public StatisticJson65 A7x7 { get; set; }
-        public AchievementsJson65 Achievements { get; set; }
-        public CommonJson65 Common { get; set; }
-        public TotalJson65 Total { get; set; }
-        public MaxJson65 Max15x15 { get; set; }
-        public MaxJson65 Max7x7 { get; set; }
+
+        public StatisticJson65 A15x15
+        {
+            get { return _a15X15; }
+            set { _a15X15 = value; }
+        }
+
+        public StatisticJson65_2 A15x15_2
+        {
+            get { return _a15X152; }
+            set { _a15X152 = value; }
+        }
+
+        public StatisticJson65 Clan
+        {
+            get { return _clan; }
+            set { _clan = value; }
+        }
+
+        public StatisticJson65_2 Clan2
+        {
+            get { return _clan2; }
+            set { _clan2 = value; }
+        }
+
+        public StatisticJson65 Company
+        {
+            get { return _company; }
+            set { _company = value; }
+        }
+
+        public StatisticJson65_2 Company2
+        {
+            get { return _company2; }
+            set { _company2 = value; }
+        }
+
+        public StatisticJson65 A7x7
+        {
+            get { return _a7X7; }
+            set { _a7X7 = value; }
+        }
+
+        public AchievementsJson65 Achievements
+        {
+            get { return _achievements; }
+            set { _achievements = value; }
+        }
+
+        public CommonJson65 Common
+        {
+            get { return _common; }
+            set { _common = value; }
+        }
+
+        public TotalJson65 Total
+        {
+            get { return _total; }
+            set { _total = value; }
+        }
+
+        public MaxJson65 Max15x15
+        {
+            get { return _max15X15; }
+            set { _max15X15 = value; }
+        }
+
+        public MaxJson65 Max7x7
+        {
+            get { return _max7X7; }
+            set { _max7X7 = value; }
+        }
 
         public IList<IList<string>> FragsList
         {

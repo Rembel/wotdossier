f1f7fe2bf954ac1442739d21a39f1ca991ec1f97
rembel <Rembel@users.noreply.github.com>
2014-01-03 09:43:30 +0000

    Tanks expectations updated

---
+++ Sources/WotDossier/Converters/Color/WN8ToColorConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace WotDossier.Converters.Color
+{
+    public class WN8ToColorConverter : IValueConverter
+    {
+        private static readonly WN8ToColorConverter defaultInstance = new WN8ToColorConverter();
+
+        public static WN8ToColorConverter Default { get { return defaultInstance; } }
+
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double? eff = value as double?;
+            if (eff != null)
+            {
+                if (eff >= 2350)
+                    return EffRangeBrushes.Purple;
+                if (eff >= 1900)
+                    return EffRangeBrushes.Blue;
+                if (eff >= 1250)
+                    return EffRangeBrushes.Green;
+                if (eff >= 900)
+                    return EffRangeBrushes.Yellow;
+                if (eff >= 600)
+                    return EffRangeBrushes.Orange;
+                if (eff >= 300)
+                    return EffRangeBrushes.Red;
+            }
+            return EffRangeBrushes.Black;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

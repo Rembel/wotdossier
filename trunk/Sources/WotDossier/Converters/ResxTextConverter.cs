9056436a8200eaf8260629399b3e50d6956c4695
Rembel <Rembel@0b894fdd-9abc-28a4-66d2-b81a71abe1db>
2014-01-22 20:57:27 +0000

    Localized Medals tooltips in replays

---
+++ trunk/Sources/WotDossier/Converters/ResxTextConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace WotDossier.Converters
+{
+    public class ResxTextConverter : IValueConverter
+    {
+        private static readonly ResxTextConverter _defaultInstance = new ResxTextConverter();
+
+        public static ResxTextConverter Default { get { return _defaultInstance; } }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                return Resources.Resources.ResourceManager.GetString(value.ToString()) ?? value;
+            }
+            return "-res not found-";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

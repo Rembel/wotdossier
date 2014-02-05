67caa93d2f142ff0eb5e97b2eada4b250fbd8437
rembel <Rembel@users.noreply.github.com>
2014-02-05 12:34:12 +0000

    0.8.11 support

---
+++ Sources/WotDossier.Applications/ViewModel/Replay/FinishReason.cs
@@ -0,0 +1,12 @@
+﻿namespace WotDossier.Applications.ViewModel.Replay
+{
+    public enum FinishReason
+    {
+        Unknown = 0,        //Unknown (not used)
+        Extermination = 1, 	//Extermination, all enemy vehicles were destroyed
+        BaseCapture = 2,    //Base capture, the enemy base was captured
+        Timeout = 3,        //Timeout, the battle time has elapsed
+        Failure = 4,         //Failure, the arena did not initialize or had an internal failure
+        Technical = 5         //Technical, the match was interrupted by a server restart or was otherwise cancelled by the server 
+    }
+}
--- Sources/WotDossier.Domain/Replay/Replay.cs
+++ Sources/WotDossier.Domain/Replay/Replay.cs
@@ -1,5 +1,4 @@
 ﻿using System.Runtime.Serialization;
-using WotDossier.Common;
 
 namespace WotDossier.Domain.Replay
 {
@@ -12,41 +11,10 @@ http://wiki.vbaddict.net
         [DataMember]
         public FirstBlock datablock_1 { get; set; }
         [DataMember]
-        public CommandResult CommandResult { get; set; }
+        public PlayerResult datablock_battle_result_plain { get; set; }
         [DataMember]
         public BattleResult datablock_battle_result { get; set; }
         [DataMember]
         public ReplayIdentify identify { get; set; }
-    }
-
-    public class ReplayIdentify
-    {
-        [DataMember]
-        public long accountDBID { get; set; }
-        [DataMember]
-        public long arenaCreateTime { get; set; }
-        [DataMember]
-        public long arenaUniqueID { get; set; }
-        [DataMember]
-        public int countryid { get; set; }
-        [DataMember]
-        public string error { get; set; }
-        [DataMember]
-        public string error_details { get; set; }
-        [DataMember]
-        public long internaluserID { get; set; }
-        [DataMember]
-        public string mapName { get; set; }
-        [DataMember]
-        public int mapid { get; set; }
-        [DataMember]
-        public string playername { get; set; }
-        [DataMember]
-        public int tankid { get; set; }
-
-        public int TankUniqueId()
-        {
-            return Utils.ToUniqueId(countryid, tankid);
-        }
     }
 }

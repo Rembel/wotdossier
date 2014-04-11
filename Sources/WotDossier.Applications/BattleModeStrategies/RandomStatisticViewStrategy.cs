9e24af9d69fff95e1319ae77bc7b1469c032ddee
Rembel <Rembel@0b894fdd-9abc-28a4-66d2-b81a71abe1db>
2014-04-11 18:15:36 +0000

    Refactoring

---
+++ Sources/WotDossier.Applications/BattleModeStrategies/RandomStatisticViewStrategy.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using WotDossier.Applications.ViewModel;
+using WotDossier.Applications.ViewModel.Rows;
+using WotDossier.Dal;
+using WotDossier.Domain.Entities;
+using WotDossier.Domain.Tank;
+
+namespace WotDossier.Applications
+{
+    public class RandomStatisticViewStrategy : StatisticViewStrategyBase
+    {
+        /// <summary>
+        /// Predicate to get tank statistic
+        /// </summary>
+        public override Func<TankJson, StatisticJson> Predicate
+        {
+            get { return tank => tank.A15x15; }
+        }
+
+        /// <summary>
+        /// To the tank statistic row.
+        /// </summary>
+        /// <param name="currentStatistic">The current statistic.</param>
+        /// <param name="prevStatisticViewModels">The previous statistic view models.</param>
+        /// <returns></returns>
+        protected override ITankStatisticRow ToTankStatisticRow(TankJson currentStatistic, List<TankJson> prevStatisticViewModels)
+        {
+            TankStatisticRowViewModel model = new TankStatisticRowViewModel(currentStatistic, prevStatisticViewModels.Any() ? prevStatisticViewModels : new List<TankJson> { TankJson.Initial });
+            return model;
+        }
+
+        /// <summary>
+        /// To the view model.
+        /// </summary>
+        /// <param name="currentStatistic">The current statistic.</param>
+        /// <returns></returns>
+        protected override PlayerStatisticViewModel ToViewModel(StatisticEntity currentStatistic)
+        {
+            return new RandomPlayerStatisticViewModel((PlayerStatisticEntity)currentStatistic);
+        }
+
+        /// <summary>
+        /// To the view model.
+        /// </summary>
+        /// <param name="currentStatistic">The current statistic.</param>
+        /// <param name="oldStatisticEntities">The old statistic entities.</param>
+        /// <returns></returns>
+        protected override PlayerStatisticViewModel ToViewModel(StatisticEntity currentStatistic, List<PlayerStatisticViewModel> oldStatisticEntities)
+        {
+            return new RandomPlayerStatisticViewModel((PlayerStatisticEntity)currentStatistic, oldStatisticEntities);
+        }
+
+        /// <summary>
+        /// Gets the statistic slices.
+        /// </summary>
+        /// <param name="repository">The repository.</param>
+        /// <param name="player">The player.</param>
+        /// <returns></returns>
+        public override List<StatisticEntity> GetStatisticSlices(DossierRepository repository, PlayerEntity player)
+        {
+            return repository.GetStatistic<PlayerStatisticEntity>(player.PlayerId).Cast<StatisticEntity>().ToList();
+        }
+    }
+}

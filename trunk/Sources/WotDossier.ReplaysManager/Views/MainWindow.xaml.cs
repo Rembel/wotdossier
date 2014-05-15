492ebedb5903956bfa5b19b20d091a222bcf1c9b
Rembel <Rembel@0b894fdd-9abc-28a4-66d2-b81a71abe1db>
2014-05-15 13:52:24 +0000

    Charts added to ReplaysManager

--- trunk/Sources/WotDossier.ReplaysManager/Views/MainWindow.xaml.cs
+++ trunk/Sources/WotDossier.ReplaysManager/Views/MainWindow.xaml.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Microsoft.Research.DynamicDataDisplay;
+using Microsoft.Research.DynamicDataDisplay.Charts;
+using Microsoft.Research.DynamicDataDisplay.Charts.Axes.GenericLocational;
 using WotDossier.Applications.View;
+using WotDossier.Dal;
+using WotDossier.Domain;
 
 namespace WotDossier.ReplaysManager
 {
@@ -23,6 +18,38 @@ namespace WotDossier.ReplaysManager
         public MainWindow()
         {
             InitializeComponent();
+
+            ConfigureChart(BattlesCountByMap);
+            ConfigureChart(BattlesWinPercentByMap);
+
+            BattlesCountByMap.Children.Add(GetMapChartVerticalAxis());
+            BattlesCountByMap.MaxY = Dictionaries.Instance.Maps.Count + 1;
+            BattlesWinPercentByMap.Children.Add(GetMapChartVerticalAxis());
+            BattlesWinPercentByMap.MaxY = Dictionaries.Instance.Maps.Count + 1;
+
+            // Enable "minimize to tray" behavior for this Window
+            MinimizeToTray.Enable(this);
+        }
+
+        private void ConfigureChart(ChartPlotter chart)
+        {
+            chart.Children.Remove(chart.DefaultContextMenu);
+            chart.Children.Remove(chart.MouseNavigation);
+            chart.Children.Remove(chart.KeyboardNavigation);
+        }
+
+        private static VerticalAxis GetMapChartVerticalAxis()
+        {
+            VerticalAxis axis = new VerticalAxis();
+
+            List<Map> list = Dictionaries.Instance.Maps.Values.ToList();
+
+            GenericLocationalLabelProvider<Map, double> labelProvider = new GenericLocationalLabelProvider<Map, double>(list, city => city.localizedmapname);
+            GenericLocationalTicksProvider<Map, double> ticksProvider = new GenericLocationalTicksProvider<Map, double>(list, city => city.mapid);
+
+            axis.LabelProvider = labelProvider;
+            axis.TicksProvider = ticksProvider;
+            return axis;
         }
     }
 }

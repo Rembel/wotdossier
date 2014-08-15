8a6a89ca62ee15d29146607ceefade48ff244d60
Rembel <Rembel@0b894fdd-9abc-28a4-66d2-b81a71abe1db>
2014-08-15 14:16:10 +0000

    Refactoring.

--- Sources/WotDossier.ReplaysManager/Views/MainWindow.xaml.cs
+++ Sources/WotDossier.ReplaysManager/Views/MainWindow.xaml.cs
@@ -1,55 +1,18 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Windows;
-using Microsoft.Research.DynamicDataDisplay;
-using Microsoft.Research.DynamicDataDisplay.Charts;
-using Microsoft.Research.DynamicDataDisplay.Charts.Axes.GenericLocational;
+﻿using System.Windows;
 using WotDossier.Applications.View;
-using WotDossier.Dal;
-using WotDossier.Domain;
 
 namespace WotDossier.ReplaysManager
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    ///     Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window, IShellView
     {
         public MainWindow()
         {
             InitializeComponent();
-
-            ConfigureChart(BattlesCountByMap);
-            ConfigureChart(BattlesWinPercentByMap);
-
-            BattlesCountByMap.Children.Add(GetMapChartVerticalAxis());
-            BattlesCountByMap.MaxY = Dictionaries.Instance.Maps.Count + 1;
-            BattlesWinPercentByMap.Children.Add(GetMapChartVerticalAxis());
-            BattlesWinPercentByMap.MaxY = Dictionaries.Instance.Maps.Count + 1;
-
             // Enable "minimize to tray" behavior for this Window
             MinimizeToTray.Enable(this);
-        }
-
-        private void ConfigureChart(ChartPlotter chart)
-        {
-            chart.Children.Remove(chart.DefaultContextMenu);
-            chart.Children.Remove(chart.MouseNavigation);
-            chart.Children.Remove(chart.KeyboardNavigation);
-        }
-
-        private static VerticalAxis GetMapChartVerticalAxis()
-        {
-            VerticalAxis axis = new VerticalAxis();
-
-            List<Map> list = Dictionaries.Instance.Maps.Values.ToList();
-
-            GenericLocationalLabelProvider<Map, double> labelProvider = new GenericLocationalLabelProvider<Map, double>(list, city => city.localizedmapname);
-            GenericLocationalTicksProvider<Map, double> ticksProvider = new GenericLocationalTicksProvider<Map, double>(list, city => city.mapid);
-
-            axis.LabelProvider = labelProvider;
-            axis.TicksProvider = ticksProvider;
-            return axis;
         }
     }
 }
--- Sources/WotDossier/Tabs/ChartsTab.xaml.cs
+++ Sources/WotDossier/Tabs/ChartsTab.xaml.cs
@@ -1,6 +1,4 @@
 ﻿using System.Windows.Controls;
-using WotDossier.Dal;
-using WotDossier.UI;
 
 namespace WotDossier.Tabs
 {
@@ -12,14 +10,6 @@ namespace WotDossier.Tabs
         public ChartsTab()
         {
             InitializeComponent();
-
-            ChartHelper.ConfigureChart(BattlesCountByMap);
-            ChartHelper.ConfigureChart(BattlesWinPercentByMap);
-
-            BattlesCountByMap.Children.Add(ChartHelper.GetMapChartVerticalAxis());
-            BattlesCountByMap.MaxY = Dictionaries.Instance.Maps.Count + 1;
-            BattlesWinPercentByMap.Children.Add(ChartHelper.GetMapChartVerticalAxis());
-            BattlesWinPercentByMap.MaxY = Dictionaries.Instance.Maps.Count + 1;
         }
     }
 }

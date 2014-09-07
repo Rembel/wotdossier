2aad53f62c0efd685eba08ee5a7f45429a1e18e4
Rembel <Rembel@0b894fdd-9abc-28a4-66d2-b81a71abe1db>
2014-09-07 09:31:54 +0000

    Prepare to add 0.9.3 support

---
+++ Sources/WotDossier.Test/ReplaysTestFixture.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using Moq;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using WotDossier.Applications;
+using WotDossier.Applications.View;
+using WotDossier.Applications.ViewModel.Replay;
+using WotDossier.Common;
+using WotDossier.Common.Extensions;
+using WotDossier.Domain.Replay;
+
+namespace WotDossier.Test
+{
+    /// <summary>
+    /// Replays tests
+    /// </summary>
+    public class ReplaysTestFixture : TestFixtureBase
+    {
+        [Test]
+        public void ReplayTest_084()
+        {
+            FileInfo cacheFile =
+                new FileInfo(Path.Combine(Environment.CurrentDirectory,
+                    @"Replays\0.8.5\20121107_1810_ussr-KV-1s_10_hills.wotreplay"));
+            Replay replay = ReplayFileHelper.ParseReplay_8_0(cacheFile);
+        }
+
+        [Test]
+        public void ReplayTest_086()
+        {
+            FileInfo cacheFile =
+                new FileInfo(Path.Combine(Environment.CurrentDirectory,
+                    @"Replays\0.8.6\20130612_0912_germany-E-100_28_desert.wotreplay"));
+            Replay replay = ReplayFileHelper.ParseReplay_8_0(cacheFile);
+        }
+
+        [Test]
+        public void ReplayTest_087()
+        {
+            FileInfo cacheFile =
+                new FileInfo(Path.Combine(Environment.CurrentDirectory,
+                    @"Replays\0.8.7\20130706_1009_ussr-T-54_73_asia_korea.wotreplay"));
+            Replay replay = ReplayFileHelper.ParseReplay_8_0(cacheFile);
+        }
+
+        [Test]
+        public void ReplayTest_088()
+        {
+            FileInfo cacheFile =
+                new FileInfo(Path.Combine(Environment.CurrentDirectory,
+                    @"Replays\0.8.8\20130908_2025_usa-M103_14_siegfried_line.wotreplay"));
+            Replay replay = ReplayFileHelper.ParseReplay_8_0(cacheFile);
+        }
+
+        [Test]
+        public void ReplayTest_089()
+        {
+            FileInfo cacheFile =
+                new FileInfo(Path.Combine(Environment.CurrentDirectory,
+                    @"Replays\0.8.9\20131016_0035_ussr-Object263_37_caucasus.wotreplay"));
+            Replay replay = ReplayFileHelper.ParseReplay_8_0(cacheFile);
+        }
+
+        [Test]
+        public void ReplayTest_0810()
+        {
+            FileInfo cacheFile =
+                new FileInfo(Path.Combine(Environment.CurrentDirectory,
+                    @"Replays\0.8.10\20131208_0156_ussr-Object_140_53_japan.wotreplay"));
+            Replay replay = ReplayFileHelper.ParseReplay_8_0(cacheFile);
+        }
+
+        [Test]
+        public void ReplayTest_0811()
+        {
+            FileInfo cacheFile =
+                new FileInfo(Path.Combine(Environment.CurrentDirectory,
+                    @"Replays\0.8.11\20140126_2109_ussr-T-54_14_siegfried_line.wotreplay"));
+
+            Replay replay = ReplayFileHelper.ParseReplay_8_11(cacheFile, true);
+            Assert.IsNotNull(replay);
+            Assert.IsNotNull(replay.datablock_battle_result);
+        }
+
+        [Test]
+        public void ReplayTest_090()
+        {
+            FileInfo cacheFile =
+                new FileInfo(Path.Combine(Environment.CurrentDirectory,
+                    @"Replays\0.9.0\13954715200495_germany_PzVI_prohorovka.wotreplay"));
+
+            Replay replay = ReplayFileHelper.ParseReplay_8_11(cacheFile, true);
+            Assert.IsNotNull(replay);
+            Assert.IsNotNull(replay.datablock_battle_result);
+        }
+
+        [Test]
+        public void ReplayTest_092()
+        {
+            FileInfo cacheFile =
+                new FileInfo(Path.Combine(Environment.CurrentDirectory,
+                    @"Replays\0.9.2\20140713_0042_usa-T57_58_73_asia_korea.wotreplay"));
+
+            Replay replay = ReplayFileHelper.ParseReplay_8_11(cacheFile, true);
+            Assert.IsNotNull(replay);
+            Assert.IsNotNull(replay.datablock_battle_result);
+        }
+
+        [Test]
+        public void ReplayTest_093()
+        {
+            FileInfo cacheFile =
+                new FileInfo(Path.Combine(Environment.CurrentDirectory,
+                    @"Replays\0.9.3\20140907_1157_usa-T57_58_73_asia_korea.wotreplay"));
+
+            Replay replay = ReplayFileHelper.ParseReplay_8_11(cacheFile, false);
+            Assert.IsNotNull(replay);
+            Assert.IsNotNull(replay.datablock_battle_result);
+            var mockView = new Mock<IReplayView>();
+            ReplayViewModel model = new ReplayViewModel(mockView.Object);
+            model.Init(replay, new PhisicalReplay(cacheFile, replay, Guid.NewGuid()));
+        }
+
+        [Test]
+        public void ReplayTest()
+        {
+            FileInfo cacheFile = new FileInfo(Path.Combine(Environment.CurrentDirectory, @"Replays\0.8.5\20121107_1810_ussr-KV-1s_10_hills.wotreplay"));
+            Replay replay = ReplayFileHelper.ParseReplay_8_11(cacheFile);
+        }
+
+        [Test]
+        public void AdvancedReplayTest()
+        {
+            FileInfo cacheFile = new FileInfo(Path.Combine(Environment.CurrentDirectory, @"Replays\0.9.1\14003587093213_ussr_Object_140_el_hallouf.wotreplay"));
+            StopWatch watch = new StopWatch();
+            watch.Reset();
+            Replay replay = ReplayFileHelper.ParseReplay_8_0(cacheFile, true);
+            Console.WriteLine(watch.PeekMs());
+
+            string serializeObject = JsonConvert.SerializeObject(replay, Formatting.Indented);
+            serializeObject.Dump(cacheFile.FullName + "_1");
+
+            watch.Reset();
+            replay = ReplayFileHelper.ParseReplay_8_11(cacheFile);
+            Console.WriteLine(watch.PeekMs());
+
+            serializeObject = JsonConvert.SerializeObject(replay, Formatting.Indented);
+            serializeObject.Dump(cacheFile.FullName + "_2");
+        }
+
+        [Test]
+        public void ReplaysFoldersSaveLoadTest()
+        {
+            ReplayFolder folder = new ReplayFolder{Name = "Parent", Path = "c:\\Parent", Folders = new ObservableCollection<ReplayFolder> {new ReplayFolder{Name = "Child", Path = "c:\\Child"}}};
+            string xml = XmlSerializer.StoreObjectInXml(folder);
+            Console.WriteLine(xml);
+
+            ReplayFolder replayFolder = XmlSerializer.LoadObjectFromXml<ReplayFolder>(xml);
+
+            Console.WriteLine(replayFolder.Folders.Count);
+        }
+    }
+}
--- Sources/WotDossier.Test/TestFixtureBase.cs
+++ Sources/WotDossier.Test/TestFixtureBase.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.ComponentModel.Composition;
 using System.IO;
@@ -10,22 +9,17 @@ using System.Text;
 using System.Web;
 using System.Xml;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using NUnit.Framework;
 using WotDossier.Applications;
 using WotDossier.Applications.Logic;
 using WotDossier.Applications.Logic.Export;
 using WotDossier.Applications.Update;
-using WotDossier.Applications.ViewModel.Replay;
 using WotDossier.Applications.ViewModel.Rows;
-using WotDossier.Common;
-using WotDossier.Common.Extensions;
 using WotDossier.Common.Reflection;
 using WotDossier.Dal;
 using WotDossier.Dal.NHibernate;
 using WotDossier.Domain.Entities;
 using WotDossier.Domain.Interfaces;
-using WotDossier.Domain.Replay;
 using WotDossier.Domain.Tank;
 using Formatting = Newtonsoft.Json.Formatting;
 
@@ -72,298 +66,6 @@ namespace WotDossier.Test
             DataProvider.CloseSession();
         }
 
-        #region Dossier cache load tests
-
-        [Test]
-        public void CacheTest_085()
-        {
-            FileInfo cacheFile = GetCacheFile("_rembel__ru", @"\CacheFiles\0.8.5\");
-            
-            List<TankJson> tanks = CacheFileHelper.ReadTanksCache(CacheFileHelper.BinaryCacheToJson(cacheFile));
-            foreach (TankJson tankJson in tanks)
-            {
-                string iconPath = string.Format(@"..\..\..\WotDossier.Resources\Images\Tanks\{0}.png",
-                                                tankJson.Description.Icon.IconId);
-                Assert.True(File.Exists(iconPath), string.Format("can't find icon {0}", tankJson.Description.Icon.IconId));
-            }
-        }
-
-        [Test]
-        public void CacheTest_086()
-        {
-            FileInfo cacheFile = GetCacheFile("_rembel__ru", @"\CacheFiles\0.8.6\");
-            List<TankJson> tanks = CacheFileHelper.ReadTanksCache(CacheFileHelper.BinaryCacheToJson(cacheFile));
-            foreach (TankJson tankJson in tanks)
-            {
-                string iconPath = string.Format(@"..\..\..\WotDossier.Resources\Images\Tanks\{0}.png",
-                                                tankJson.Description.Icon.IconId);
-                Assert.True(File.Exists(iconPath), string.Format("can't find icon {0}", tankJson.Description.Icon.IconId));
-            }
-        }
-
-        [Test]
-        public void CacheTest_087()
-        {
-            FileInfo cacheFile = GetCacheFile("_rembel__ru", @"\CacheFiles\0.8.7\");
-            List<TankJson> tanks = CacheFileHelper.ReadTanksCache(CacheFileHelper.BinaryCacheToJson(cacheFile));
-            foreach (TankJson tankJson in tanks)
-            {
-                string iconPath = string.Format(@"..\..\..\WotDossier.Resources\Images\Tanks\{0}.png",
-                                                tankJson.Description.Icon.IconId);
-                Assert.True(File.Exists(iconPath), string.Format("can't find icon {0}", tankJson.Description.Icon.IconId));
-            }
-        }
-
-        [Test]
-        public void CacheTest_088()
-        {
-            FileInfo cacheFile = GetCacheFile("_rembel__ru", @"\CacheFiles\0.8.8\");
-            List<TankJson> tanks = CacheFileHelper.ReadTanksCache(CacheFileHelper.BinaryCacheToJson(cacheFile));
-            foreach (TankJson tankJson in tanks)
-            {
-                string iconPath = string.Format(@"..\..\..\WotDossier.Resources\Images\Tanks\{0}.png",
-                                                tankJson.Description.Icon.IconId);
-                Assert.True(File.Exists(iconPath), string.Format("can't find icon {0}", tankJson.Description.Icon.IconId));
-            }
-        }
-
-        [Test]
-        public void CacheTest_089()
-        {
-            FileInfo cacheFile = GetCacheFile("_rembel__ru", @"\CacheFiles\0.8.9\");
-            List<TankJson> tanks = CacheFileHelper.ReadTanksCache(CacheFileHelper.BinaryCacheToJson(cacheFile));
-            foreach (TankJson tankJson in tanks)
-            {
-                string iconPath = string.Format(@"..\..\..\WotDossier.Resources\Images\Tanks\{0}.png",
-                                                tankJson.Description.Icon.IconId);
-                Assert.True(File.Exists(iconPath), string.Format("can't find icon {0}", tankJson.Description.Icon.IconId));
-            }
-        }
-
-        [Test]
-        public void CacheTest_0810()
-        {
-            FileInfo cacheFile = GetCacheFile("_rembel__ru", @"\CacheFiles\0.8.10\");
-            List<TankJson> tanks = CacheFileHelper.ReadTanksCache(CacheFileHelper.BinaryCacheToJson(cacheFile));
-            foreach (TankJson tankJson in tanks)
-            {
-                string iconPath = string.Format(@"..\..\..\WotDossier.Resources\Images\Tanks\{0}.png",
-                                                tankJson.Description.Icon.IconId);
-                Assert.True(File.Exists(iconPath), string.Format("can't find icon {0}", tankJson.Description.Icon.IconId));
-            }
-        }
-
-        [Test]
-        public void CacheTest_0811()
-        {
-            FileInfo cacheFile = GetCacheFile("_rembel__ru", @"\CacheFiles\0.8.11\");
-            List<TankJson> tanks = CacheFileHelper.ReadTanksCache(CacheFileHelper.BinaryCacheToJson(cacheFile));
-            foreach (TankJson tankJson in tanks)
-            {
-                string iconPath = string.Format(@"..\..\..\WotDossier.Resources\Images\Tanks\{0}.png",
-                                                tankJson.Description.Icon.IconId);
-                Assert.True(File.Exists(iconPath), string.Format("can't find icon {0}", tankJson.Description.Icon.IconId));
-            }
-        }
-
-        [Test]
-        public void CacheTest_090()
-        {
-            FileInfo cacheFile = GetCacheFile("_rembel__ru", @"\CacheFiles\0.9.0\");
-            List<TankJson> tanks = CacheFileHelper.ReadTanksCache(CacheFileHelper.BinaryCacheToJson(cacheFile));
-            foreach (TankJson tankJson in tanks)
-            {
-                string iconPath = string.Format(@"..\..\..\WotDossier.Resources\Images\Tanks\{0}.png",
-                                                tankJson.Description.Icon.IconId);
-                Assert.True(File.Exists(iconPath), string.Format("can't find icon {0}", tankJson.Description.Icon.IconId));
-            }
-        }
-
-        [Test]
-        public void CacheTest_091()
-        {
-            FileInfo cacheFile = GetCacheFile("_rembel__ru", @"\CacheFiles\0.9.1\");
-            List<TankJson> tanks = CacheFileHelper.ReadTanksCache(CacheFileHelper.BinaryCacheToJson(cacheFile));
-            foreach (TankJson tankJson in tanks)
-            {
-                string iconPath = string.Format(@"..\..\..\WotDossier.Resources\Images\Tanks\{0}.png",
-                                                tankJson.Description.Icon.IconId);
-                Assert.True(File.Exists(iconPath), string.Format("can't find icon {0}", tankJson.Description.Icon.IconId));
-            }
-        }
-
-        [Test]
-        public void CacheTest_092()
-        {
-            FileInfo cacheFile = GetCacheFile("_rembel__ru", @"\CacheFiles\0.9.2\");
-            List<TankJson> tanks = CacheFileHelper.ReadTanksCache(CacheFileHelper.BinaryCacheToJson(cacheFile));
-            foreach (TankJson tankJson in tanks)
-            {
-                string iconPath = string.Format(@"..\..\..\WotDossier.Resources\Images\Tanks\{0}.png",
-                                                tankJson.Description.Icon.IconId);
-                Assert.True(File.Exists(iconPath), string.Format("can't find icon {0}", tankJson.Description.Icon.IconId));
-            }
-        }
-
-        #endregion
-        
-        #region Replays tests
-
-        [Test]
-        public void ReplayTest_084()
-        {
-            FileInfo cacheFile =
-                new FileInfo(Path.Combine(Environment.CurrentDirectory,
-                                          @"Replays\0.8.5\20121107_1810_ussr-KV-1s_10_hills.wotreplay"));
-            Replay replay = ReplayFileHelper.ParseReplay_8_0(cacheFile);
-        }
-
-        [Test]
-        public void ReplayTest_086()
-        {
-            FileInfo cacheFile =
-                new FileInfo(Path.Combine(Environment.CurrentDirectory,
-                                          @"Replays\0.8.6\20130612_0912_germany-E-100_28_desert.wotreplay"));
-            Replay replay = ReplayFileHelper.ParseReplay_8_0(cacheFile);
-        }
-
-        [Test]
-        public void ReplayTest_087()
-        {
-            FileInfo cacheFile =
-                new FileInfo(Path.Combine(Environment.CurrentDirectory,
-                                          @"Replays\0.8.7\20130706_1009_ussr-T-54_73_asia_korea.wotreplay"));
-            Replay replay = ReplayFileHelper.ParseReplay_8_0(cacheFile);
-        }
-
-        [Test]
-        public void ReplayTest_088()
-        {
-            FileInfo cacheFile =
-                new FileInfo(Path.Combine(Environment.CurrentDirectory,
-                                          @"Replays\0.8.8\20130908_2025_usa-M103_14_siegfried_line.wotreplay"));
-            Replay replay = ReplayFileHelper.ParseReplay_8_0(cacheFile);
-        }
-
-        [Test]
-        public void ReplayTest_089()
-        {
-            FileInfo cacheFile =
-                new FileInfo(Path.Combine(Environment.CurrentDirectory,
-                                          @"Replays\0.8.9\20131016_0035_ussr-Object263_37_caucasus.wotreplay"));
-            Replay replay = ReplayFileHelper.ParseReplay_8_0(cacheFile);
-        }
-
-        [Test]
-        public void ReplayTest_0810()
-        {
-            FileInfo cacheFile =
-                new FileInfo(Path.Combine(Environment.CurrentDirectory,
-                                          @"Replays\0.8.10\20131208_0156_ussr-Object_140_53_japan.wotreplay"));
-            Replay replay = ReplayFileHelper.ParseReplay_8_0(cacheFile);
-        }
-
-        [Test]
-        public void ReplayTest_0811()
-        {
-            FileInfo cacheFile =
-                new FileInfo(Path.Combine(Environment.CurrentDirectory,
-                                          @"Replays\0.8.11\20140126_2109_ussr-T-54_14_siegfried_line.wotreplay"));
-
-            Replay replay = ReplayFileHelper.ParseReplay_8_11(cacheFile, true);
-            Assert.IsNotNull(replay);
-            Assert.IsNotNull(replay.datablock_battle_result);
-        }
-        
-        [Test]
-        public void ReplayTest_090()
-        {
-            FileInfo cacheFile =
-                new FileInfo(Path.Combine(Environment.CurrentDirectory,
-                                          @"Replays\0.9.0\13954715200495_germany_PzVI_prohorovka.wotreplay"));
-
-            Replay replay = ReplayFileHelper.ParseReplay_8_11(cacheFile, true);
-            Assert.IsNotNull(replay);
-            Assert.IsNotNull(replay.datablock_battle_result);
-        }
-
-        [Test]
-        public void ReplayTest_092()
-        {
-            FileInfo cacheFile =
-                new FileInfo(Path.Combine(Environment.CurrentDirectory,
-                                          @"Replays\0.9.2\20140713_0042_usa-T57_58_73_asia_korea.wotreplay"));
-
-            Replay replay = ReplayFileHelper.ParseReplay_8_11(cacheFile, true);
-            Assert.IsNotNull(replay);
-            Assert.IsNotNull(replay.datablock_battle_result);
-        }
-
-        [Test]
-        public void ReplayTest()
-        {
-            FileInfo cacheFile = new FileInfo(Path.Combine(Environment.CurrentDirectory, @"Replays\0.8.5\20121107_1810_ussr-KV-1s_10_hills.wotreplay"));
-            Replay replay = ReplayFileHelper.ParseReplay_8_11(cacheFile);
-        }
-
-        #endregion
-
-        #region Help methods
-
-        /// <summary>
-        /// Gets the cache file.
-        /// </summary>
-        /// <param name="playerId">The player id.</param>
-        /// <returns>null if there is no any dossier cache file for specified player</returns>
-        public static FileInfo GetCacheFile(string playerId, string folder)
-        {
-            FileInfo cacheFile = null;
-
-            string[] files = new string[0];
-
-            files = Directory.GetFiles(Environment.CurrentDirectory + folder, "*.dat");
-
-            if (!files.Any())
-            {
-                return null;
-            }
-
-            foreach (string file in files)
-            {
-                FileInfo info = new FileInfo(file);
-
-                if (GetPlayerName(info).Equals(playerId, StringComparison.InvariantCultureIgnoreCase))
-                {
-                    if (cacheFile == null)
-                    {
-                        cacheFile = info;
-                    }
-                    else if (cacheFile.LastWriteTime < info.LastWriteTime)
-                    {
-                        cacheFile = info;
-                    }
-                }
-            }
-            return cacheFile;
-        }
-
-        /// <summary>
-        /// Gets the name of the player from name of dossier cache file.
-        /// </summary>
-        /// <param name="cacheFile">The cache file in base32 format. Example of decoded filename - login-ct-p1.worldoftanks.com:20015;_Rembel__RU</param>
-        /// <returns></returns>
-        public static string GetPlayerName(FileInfo cacheFile)
-        {
-            Base32Encoder encoder = new Base32Encoder();
-            string str = cacheFile.Name.Replace(cacheFile.Extension, string.Empty);
-            byte[] decodedFileNameBytes = encoder.Decode(str.ToLowerInvariant());
-            string decodedFileName = Encoding.UTF8.GetString(decodedFileNameBytes);
-            string playerName = decodedFileName.Split(';')[1];
-            return playerName;
-        }
-
-        #endregion
-        
         [Test]
         //TODO: move to api 2.0
         public void LoginTest()
@@ -548,18 +250,6 @@ namespace WotDossier.Test
         }
 
         [Test]
-        public void ReplaysFoldersSaveLoadTest()
-        {
-            ReplayFolder folder = new ReplayFolder{Name = "Parent", Path = "c:\\Parent", Folders = new ObservableCollection<ReplayFolder> {new ReplayFolder{Name = "Child", Path = "c:\\Child"}}};
-            string xml = XmlSerializer.StoreObjectInXml(folder);
-            Console.WriteLine(xml);
-
-            ReplayFolder replayFolder = XmlSerializer.LoadObjectFromXml<ReplayFolder>(xml);
-
-            Console.WriteLine(replayFolder.Folders.Count);
-        }
-
-        [Test]
         public void NominalDamageTest()
         {
             XmlDocument document = new XmlDocument();
@@ -608,65 +298,10 @@ namespace WotDossier.Test
         }
 
         [Test]
-        public void CacheTest()
-        {
-            FileInfo cacheFile = GetCacheFile("_rembel__ru", @"\CacheFiles\0.8.9\");
-            List<TankJson> tanks = CacheFileHelper.ReadTanksCache(CacheFileHelper.BinaryCacheToJson(cacheFile));
-
-            foreach (var group in tanks.GroupBy(x => x.Common.type))
-            {
-                Console.WriteLine();
-                Console.WriteLine("Battles - " + group.Sum(x => x.A15x15.battlesCount));
-
-                foreach (TankJson tankJson in group.OrderBy(x => x.Common.type).ThenByDescending(x => x.A15x15.battlesCount))
-                {
-                    Console.WriteLine("15x15-{1}\t\tclan-{2}\tcompany-{3}\t7x7-{4}\t\tTank - {0} - {5}", tankJson.Common.tanktitle, GetCount(tankJson.A15x15), GetCount(tankJson.Clan), GetCount(tankJson.Company), GetCount(tankJson.A7x7), tankJson.Common.basedonversion);
-                }
-            }
-
-            Console.WriteLine("battles count cache -" + tanks.Sum(x => x.A15x15.battlesCount));
-            Console.WriteLine("last battle -" + tanks.Max(x => x.Common.lastBattleTimeR));
-            Console.WriteLine("tanks count cache - " + tanks.Count);
-
-            string tanksRequest = new Uri(
-                "http://api.worldoftanks.ru/wot/account/tanks/?application_id=19779cdf6e8aab1a8c99e1261274f13b&access_token=ab2994337c9f5b09d0b7f372001f17c45f0b4af8&account_id=1749450")
-                .Get();
-
-            var parsedData = JsonConvert.DeserializeObject<JObject>(tanksRequest);
-
-            JArray array = (JArray)parsedData["data"]["1749450"];
-            Console.WriteLine("tanks count server - " + array.Count());
-            Console.WriteLine("battles count server - " + array.Sum(x => x["statistics"]["all"]["battles"].Value<int>()));
-
-            foreach (TankJson tankJson in tanks)
-            {
-                TankServerInfo serverInfo = Dictionaries.Instance.ServerTanks.Values.FirstOrDefault(x => x.name.EndsWith(tankJson.Description.Icon.IconOrig));
-
-                if (serverInfo != null)
-                {
-                    JToken token = array.FirstOrDefault(x => x["tank_id"].Value<int>() == serverInfo.tank_id);
-                    if (token == null)
-                    {
-                        Console.WriteLine(tankJson.Description.Title);
-                    }
-                }
-            }
-        }
-
-        private string GetCount(StatisticJson a15X15)
-        {
-            if (a15X15 != null)
-            {
-                return a15X15.battlesCount.ToString("D3");
-            }
-            return "000";
-        }
-
-        [Test]
         public void CsvExportProviderTest()
         {
             CsvExportProvider provider = new CsvExportProvider();
-            FileInfo cacheFile = GetCacheFile("_rembel__ru", @"\CacheFiles\0.8.9\");
+            FileInfo cacheFile = CacheTestFixture.GetCacheFile("_rembel__ru", @"\CacheFiles\0.8.9\");
             List<TankJson> tanks = CacheFileHelper.ReadTanksCache(CacheFileHelper.BinaryCacheToJson(cacheFile));
             List<RandomBattlesTankStatisticRowViewModel> list = tanks.Select(x => new RandomBattlesTankStatisticRowViewModel(x)).ToList();
             provider.Export(list, new List<Type>{typeof(IStatisticBattles), typeof(IStatisticFrags)});
@@ -676,7 +311,7 @@ namespace WotDossier.Test
         public void appSpotTest()
         {
             AppSpotUploader uploader = new AppSpotUploader();
-            FileInfo cacheFile = GetCacheFile("_rembel__ru", @"\CacheFiles\0.8.9\");
+            FileInfo cacheFile = CacheTestFixture.GetCacheFile("_rembel__ru", @"\CacheFiles\0.8.9\");
             long id = uploader.Upload(cacheFile);
             uploader.Update(cacheFile, id);
         }
@@ -706,26 +341,6 @@ namespace WotDossier.Test
             {
                 Console.WriteLine("PiercedReceived [{0}] - BattlesCount [{1}]", tankStatisticRow.PiercedReceived, tankStatisticRow.BattlesCount);
             }
-        }
-
-        [Test]
-        public void AdvancedReplayTest()
-        {
-            FileInfo cacheFile = new FileInfo(Path.Combine(Environment.CurrentDirectory, @"Replays\0.9.1\14003587093213_ussr_Object_140_el_hallouf.wotreplay"));
-            StopWatch watch = new StopWatch();
-            watch.Reset();
-            Replay replay = ReplayFileHelper.ParseReplay_8_0(cacheFile, true);
-            Console.WriteLine(watch.PeekMs());
-
-            string serializeObject = JsonConvert.SerializeObject(replay, Formatting.Indented);
-            serializeObject.Dump(cacheFile.FullName + "_1");
-
-            watch.Reset();
-            replay = ReplayFileHelper.ParseReplay_8_11(cacheFile);
-            Console.WriteLine(watch.PeekMs());
-
-            serializeObject = JsonConvert.SerializeObject(replay, Formatting.Indented);
-            serializeObject.Dump(cacheFile.FullName + "_2");
         }
 
         [Test]

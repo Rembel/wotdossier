8e5d1fbe3ef2a8222f7f2ffb8c5261cdaa795fdb
Rembel <Rembel@gmail.com>
2015-02-11 00:09:45 +0300

    Fix cache load error

---
+++ Sources/WotDossier.Update/Scripts/2015021001.sql
@@ -0,0 +1,3 @@
+﻿Delete from PlayerAchievements where Id in (select AchievementsId from PlayerStatistic where date(Updated) in (date('2015-02-10'), date('2015-02-11')));
+delete from PlayerStatistic where date(Updated) in (date('2015-02-10'), date('2015-02-11'));
+delete from TankStatistic where date(Updated) in (date('2015-02-10'), date('2015-02-11'));
